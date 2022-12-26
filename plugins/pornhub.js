8228a25668d4c7c5b7cc45f36d429a414171d78d
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-12-27 03:47:03 +0700

    Update pornhub.js

--- plugins/pornhub.js
+++ plugins/pornhub.js
@@ -4,7 +4,7 @@ let response = args.join(' ').split('|')
   if (!args[0]) throw 'Teks Nya Banh?\nContoh :\!pornhub tes|tes'
   m.reply('Proses...')
   let res = `https://api.lolhuman.xyz/api/textprome2/pornhub?apikey=64333e1c050ced435defe154&text1=${response[0]}&text2=${response[1]}`
-  conn.sendFile(m.chat, res, 'pornhub.jpg', `*Done Banh*\n_©by ᴹᴿ᭄ King Of Bear ×፝֟͜×_`, m, false)
+  conn.sendFile(m.chat, res, 'pornhub.jpg', `*Done Banh*\n_©by SaxiaMD`, m, false)
 }
 handler.help = ['pornhub'].map(v => v + ' <text1>|<teks2>')
 handler.tags = ['maker']
