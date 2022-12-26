b22df58452d02c15eda41cf7260114190c11792c
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-12-27 00:11:08 +0700

    Update __new-_antiCall.js

--- plugins/__new-_antiCall.js
+++ plugins/__new-_antiCall.js
@@ -14,7 +14,7 @@ export async function all(m) {
 	if(!setting.anticall) return 
 	
 	if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO)) {
-		await this.reply(m.chat, 'kamu Di blockir oleh bot karena telah melanggar aturan bot\n\n*📮Dilarang menelepon Bot!*', null)
+		await this.reply(m.chat, 'Jangan Call Xia lah, Cht Owner biar di bukain\n\n*📮Dilarang menelepon Bot!*', null)
 		await delay(1000)
 		await this.updateBlockStatus(m.chat, "block")
 	}
