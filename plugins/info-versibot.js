2f5f7a1c8a06fe7b60d0a3756fde8914c285647d
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-12-27 01:26:27 +0700

    Update info-versibot.js

--- plugins/info-versibot.js
+++ plugins/info-versibot.js
@@ -1,14 +1,14 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-  const ultah = new Date('Februari 2 2023 00:00:01')
+  const ultah = new Date('Februari 4 2023 00:00:01')
     const sekarat = new Date().getTime() 
     const Kurang = ultah - sekarat
     const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
     const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
     const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
     const detek = Math.floor( Kurang % (1000 * 60) / 1000)
-  let info = `Bot ini menggunakan script by\n ᴹᴿ᭄ PutraModz ×፝֟͜×\n\n*Version:* Private Edition V3`
+  let info = `Bot ini menggunakan script by\n ©Saxia˘Botz\n\n*Version:* Version 1`
 
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 const message = {
@@ -20,13 +20,13 @@ const message = {
                 {
                     urlButton: {
                         displayText: 'Beli Sc? Klik',
-                        url: 'https://wa.me/6282214729677'
+                        url: 'https://wa.me/6285641142178'
                     }
                 },
                 {
                     urlButton: {
-                        displayText: 'Ke IG GW',
-                        url: 'https://Instragram.com/l4c1f2r',
+                        displayText: 'Website',
+                        url: 'https://bit.ly/SaxiaShop',
                     }
                 },
             ]
@@ -34,13 +34,13 @@ const message = {
         conn.sendMessage(m.chat, message)
 conn.reply(m.chat, info, m, {
 contextInfo: { externalAdReply :{ showAdAttribution: true,
-                        sourceUrl: 'https://youtu.be/ALQiRR1nzLI',
+                        sourceUrl: 'https://chat.whatsapp.com/GeWPLmclHaVHsF0GymCcJz',
                         mediaType: 2,
-                        description: `⚘ ᴄʀᴇᴀᴛᴇ ᴹᴿ᭄ PutraModz ×፝֟͜×`,
+                        description: `⚘ ᴄʀᴇᴀᴛᴇ ©Saxia˘Botz`,
                         title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
-                        body: `⚘ ᴄʀᴇᴀᴛᴇ by ᴹᴿ᭄ PutraModz ×፝֟͜×`,          previewType: 0,
+                        body: `⚘ ᴄʀᴇᴀᴛᴇ by SaxiaBotz`,          previewType: 0,
                         thumbnail: await (await fetch(giflogo2)).buffer(),
-                        mediaUrl: 'https://youtu.be/ALQiRR1nzLI'
+                        mediaUrl: 'https://Bit,ly/SaxiaShop'
                         
                       }}
 })
