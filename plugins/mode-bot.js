60157fa2aa34d1468f5e4afffecd9011d938f968
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-12-27 02:03:29 +0700

    Update mode-bot.js

--- plugins/mode-bot.js
+++ plugins/mode-bot.js
@@ -11,14 +11,14 @@ let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
- await conn.sendButton(m.chat, `*Hai Ngab*
-ɪ'ᴍ ɴᴇxʙᴏᴛᴢ ʏᴀɴɢ ᴅɪ ʙᴜᴀᴛ ɢᴀᴋ ɴɪᴀᴛ ᴏʟᴇʜ ᴘᴜᴛʀᴀᴍᴏᴅz ᴋᴀʀᴇɴᴀ ᴍᴀʟᴇꜱ ʀᴇᴄᴏᴅᴇ ᴊᴀᴅɪ ᴘᴀᴋᴇ ᴀᴊᴀ ᴀᴘᴀ ʏᴀɴɢ ᴀᴅᴀ ʏᴀʜ.
+ await conn.sendButton(m.chat, `*👋 Hai Kak*
+Aku SaxiaBotz Whatsapp Yang di buat oleh Team Saxia. Shilakan Pakai ya kak Jangan lupa join gc Xia.
 Kalau Ada Yg Error Bisa Langsung Report Ke Owner.\n`,wm + '\n\n' + botdate, thumbdoc, [['Aʟʟ Mᴇɴᴜ','.? all'],['Lɪsᴛ Mᴇɴᴜ','.siuuu']], m, {
 contextInfo: { externalAdReply :{
                         mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
-                        title: `A PUTRA🎧 : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
+                        title: `「 ©Saxia˘Botz 」`,
                         body: `Join Grup`,          previewType: 0,
                         thumbnail: await (await fetch(`https://i.ibb.co/jbHnwDr/20221218-011857.jpg`)).buffer(),
                         sourceUrl: 'https://chat.whatsapp.com/FR1KfE3OTcQCayiabos8Ff'
