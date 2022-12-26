4f49daae56c60027c5dfad380c4274b9f98fa532
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-12-27 00:46:31 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,12 +3,12 @@ let handler = async (m, { conn, text, usedPrefix }) => {
 
     let img = fs.readFileSync('./thumbnail.jpg');
     let img2 = `https://telegra.ph/file/a5eb5169ab1330a537e0b.jpg`
-    return conn.sendButton(m.chat, 'Donasi Pm Owner aja.\n❏  :v', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
+    return conn.sendButton(m.chat, 'Donasi? Chek Katalog.\n❏  :v', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
         contextInfo: {
             externalAdReply: {
-                mediaUrl: "https://instagram.com/l4c1f2r",
+                mediaUrl: "https://bit.ly/SaxiaShop",
                 mediaType: "VIDEO",
-                title: 'Donasi To PutraModz',
+                title: 'Donasi Buat Xia Yok ^0^',
                 body: bottime,
                 previewType: 0,
                 thumbnail: img
